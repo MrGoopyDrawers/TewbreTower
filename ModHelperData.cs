9a8fd7482a65cefd4666ef5139e652a364796546
MrGoopyDrawers <107856947+MrGoopyDrawers@users.noreply.github.com>
2023-06-08 09:05:31 -0400

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,7 +3,7 @@ namespace TewbreTower;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "36.1";
-    public const string Version = "1.0.0";
+    public const string Version = "1.0.1";
     public const string Name = "Tewbre";
 
     public const string Description = "Adds tewbre to the game";
